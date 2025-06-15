02753ad1e4a10f8c593cf5685c7effe092448640
Koryagin Nazar <nazarkoryagin581@gmail.com>
2025-06-15 22:48:38 +0000

    Initialize FastAPI application with middleware and health check endpoint

---
+++ app/main.py
@@ -0,0 +1,41 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+
+from core.config import settings
+from fastapi import FastAPI, status, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.responses import ORJSONResponse
+from loguru import logger
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Starting up application...")
+    yield
+    logger.info("Shutting down application...")
+
+
+app = FastAPI(default_response_class=ORJSONResponse, lifespan=lifespan)
+
+app.add_middleware(GZipMiddleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.get("/health", status_code=status.HTTP_204_NO_CONTENT)(lambda: None)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.api.host,
+        port=settings.api.port,
+        workers=settings.api.workers,
+        reload=settings.development,
+    )
