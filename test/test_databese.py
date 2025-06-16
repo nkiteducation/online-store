1aaa461118fe7068e00750ae117e4f935f88c0d4
Koryagin Nazar <nazarkoryagin581@gmail.com>
2025-06-16 02:15:30 +0000

    Improve code formatting and readability in database test file

--- test/test_databese.py
+++ test/test_databese.py
@@ -2,6 +2,7 @@ import pytest
 
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+
 from app.database.models import metadata
 
 
@@ -11,10 +12,12 @@ async def test_database_connection(session: async_sessionmaker[AsyncSession]):
         result = await db_session.execute(text("SELECT 1"))
         assert result.scalar() == 1
 
+
 @pytest.mark.asyncio
 async def test_chac_database(session: async_sessionmaker[AsyncSession]):
     async with session() as db_session:
-        result = await db_session.execute(text("SELECT name FROM sqlite_master WHERE type='table';"))
+        result = await db_session.execute(
+            text("SELECT name FROM sqlite_master WHERE type='table';")
+        )
         tables = result.scalars().all()
         assert list(metadata.tables.keys()) == tables
-        
