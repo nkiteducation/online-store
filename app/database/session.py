0817f5d247babb5d8987e41ff91e6743ca2c3259
Koryagin Nazar <nazarkoryagin581@gmail.com>
2025-06-16 01:16:10 +0000

    Add SessionManager class for asynchronous database session management

---
+++ app/database/session.py
@@ -0,0 +1,36 @@
+import asyncio
+
+from loguru import logger
+from sqlalchemy.ext.asyncio import (
+    async_scoped_session,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from core.config import settings
+
+
+class SessionManager:
+    def __init__(self, database_url: str, **engine_kwargs: dict):
+        self.engine = create_async_engine(
+            database_url,
+            future=True,
+            **engine_kwargs,
+        )
+        self.session_local = async_sessionmaker(
+            bind=self.engine, expire_on_commit=False
+        )
+        self.scoped_session = async_scoped_session(
+            self.session_local, scopefunc=asyncio.current_task
+        )
+
+    async def dispose(self):
+        await self.engine.dispose()
+
+
+session_manager = SessionManager(
+    database_url=settings.database.url.get(),
+    pool_size=settings.database.poolSize,
+    max_overflow=settings.database.maxOverflow,
+    pool_timeout=settings.database.poolTimeout,
+)
