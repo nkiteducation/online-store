a3b0bda135f0afc9ffc52de64fc5929ed15afb43
Koryagin Nazar <nazarkoryagin581@gmail.com>
2025-06-24 15:13:03 +0000

    Refactor router initialization to separate dependencies for clarity

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -5,10 +5,10 @@ from api.security.dependencies import htttp_bearer
 from fastapi import Depends
 from fastapi import APIRouter, status
 
-router = APIRouter(prefix="/v1", dependencies=[Depends(htttp_bearer)])
+router = APIRouter(prefix="/v1")
 
 router.get("/health", status_code=status.HTTP_204_NO_CONTENT)(lambda: None)
 
 router.include_router(security)
 router.include_router(products)
-router.include_router(admin)
+router.include_router(admin, dependencies=[Depends(htttp_bearer)])
