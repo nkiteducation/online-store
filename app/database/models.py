74892fde1aa79df5b8002d0b02e8b3518eec52e8
Koryagin Nazar <nazarkoryagin581@gmail.com>
2025-06-25 09:38:58 +0000

    Reorganize imports and enhance relationship definitions in models.py for clarity

--- app/database/models.py
+++ app/database/models.py
@@ -1,15 +1,17 @@
-from decimal import Decimal
 import enum
+from decimal import Decimal
+from uuid import UUID
 
 from database.mixin import TimestampMixin, UUIDMixin
-from sqlalchemy import Enum as SQLEnum, Numeric
-from sqlalchemy import MetaData
+from sqlalchemy import Enum as SQLEnum
+from sqlalchemy import ForeignKey, Integer, MetaData, Numeric
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
     declared_attr,
     mapped_column,
+    relationship,
 )
 
 metadata = MetaData(
@@ -42,11 +44,16 @@ class User(CoreModel, UUIDMixin, TimestampMixin):
     name: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str] = mapped_column(unique=True)
     password: Mapped[bytes]
-
     is_active: Mapped[bool] = mapped_column(default=True)
     role: Mapped[Role] = mapped_column(
         SQLEnum(Role, name="enum_user_role"),
         default=Role.USER,
+    )
+
+    cart_items: Mapped[list["Cart"]] = relationship(
+        "Cart",
+        back_populates="user",
+        cascade="all, delete-orphan",
     )
 
 
@@ -54,3 +61,30 @@ class Product(CoreModel, UUIDMixin, TimestampMixin):
     name: Mapped[str]
     description: Mapped[str]
     price: Mapped[Decimal] = mapped_column(Numeric(10, 2))
+
+    in_carts: Mapped[list["Cart"]] = relationship(
+        "Cart",
+        back_populates="product",
+        cascade="all, delete-orphan",
+    )
+
+
+class Cart(CoreModel, UUIDMixin, TimestampMixin):
+    user_id: Mapped[UUID] = mapped_column(
+        ForeignKey("user.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    product_id: Mapped[UUID] = mapped_column(
+        ForeignKey("product.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    quantity: Mapped[int] = mapped_column(Integer, default=1)
+
+    user: Mapped["User"] = relationship(
+        "User",
+        back_populates="cart_items",
+    )
+    product: Mapped["Product"] = relationship(
+        "Product",
+        back_populates="in_carts",
+    )
