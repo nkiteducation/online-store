57c7019ba0b6b5c4d4d3f7228dde0d627ea9c276
Koryagin Nazar <nazarkoryagin581@gmail.com>
2025-06-25 09:49:51 +0000

    Add user router to API initialization for enhanced endpoint management

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,6 +2,7 @@ from api.admin.endpoint import router as admin
 from api.products.endpoint import router as products
 from api.security.dependencies import htttp_bearer
 from api.security.endpoint import router as security
+from api.user.endpoint import router as user
 from fastapi import APIRouter, Depends, status
 
 router = APIRouter(prefix="/v1")
@@ -11,3 +12,4 @@ router.get("/health", status_code=status.HTTP_204_NO_CONTENT)(lambda: None)
 router.include_router(security)
 router.include_router(products)
 router.include_router(admin, dependencies=[Depends(htttp_bearer)])
+router.include_router(user, dependencies=[Depends(htttp_bearer)])
